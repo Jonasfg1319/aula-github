b715467ee48bb8a8aaf4f2bf133e86c6d5100ca5
aprdev <jonasfg1319@hotmail.com>
2019-08-23 11:56:12 -0300

    atualização

--- src/program/MainClass.java
+++ src/program/MainClass.java
@@ -1,11 +1,23 @@
 package program;
-
+import java.util.Scanner;
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     
-		System.out.println("Hello World!!!");
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		System.out.println(Soma(n1,n2));
+		
+		sc.close();
+	}
+	
+	public static int Soma(int n1, int n2) 
+	{
+		return n1 + n2;
 	}
 
 }
