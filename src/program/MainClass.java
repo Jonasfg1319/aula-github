c6caa87a3bf91c58cda017161c3bb4133154ff6c
aprdev <jonasfg1319@hotmail.com>
2019-08-24 05:14:10 -0300

    atualização notação posicional

--- src/program/MainClass.java
+++ src/program/MainClass.java
@@ -46,15 +46,20 @@ public class MainClass {
 	
 	public void Notacao_posicional(int base1, int base2) 
 	{
-		//Ainda em planejamento
-		System.out.println("Quantos algarismos tem o numero?");
-		int alg = sc.nextInt();
-		System.out.println("Digite o numero que você quer converter um numero de cada vez");
-		int[] vect = new int[alg];
 	    double som = 0;
-	    double mult = 0;
-	    int x = 0;
-	    	for(int i = 0; i < vect.length; i++) 
+		double mult = 0;
+		int x = 0;
+		
+		System.out.println("Quantos algarismos tem o numero?");
+		
+		int alg = sc.nextInt();
+		
+		System.out.println("Digite o numero que você quer converter um numero de cada vez");
+		
+		int[] vect = new int[alg];
+	  
+	    	
+	        for(int i = 0; i < vect.length; i++) 
 	    	{
 	    		System.out.println((i+1)+"# :"+" Numero?");
 	    		vect[i] = sc.nextInt();
@@ -70,20 +75,22 @@ public class MainClass {
 	    		x++;
 	    		
 	    	}
-	    	System.out.println("O resultado é "+som);
+	    	System.out.println("O resultado é "+som+" na base "+base2);
 	        
 	    
 	   }
 	
 	public void Divisao_sucessiva(int num,int base) 
-	{
+		{
 		List<Integer> lista = new ArrayList<>();
 		double resto;
 		double temp = 1;
 		double temp2;
 		temp = num / base;
 		resto = num % base;
+		
 		System.out.println(temp);
+	    
 		while(temp > 1) 
 		{
 			lista.add((int)resto);
